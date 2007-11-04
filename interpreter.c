2c53e4dc771d6766325b0d7370b3a80234a5bb7b
Maks Verver <maksverver@geocities.com>
2007-11-04 21:51:24 +0100

    Fixed interpreter bug (and moved some code around).
    The interpreter now does not request input when a cursor is in
    input mode but its next instruction is invalid. This occurs
    only if the next instruction is "^" and the data pointer is
    already on the topmost row.

--- interpreter.c
+++ interpreter.c
@@ -10,13 +10,36 @@
 const int DR[4] = {  0, +1,  0, -1 };
 const int DC[4] = { +1,  0, -1,  0 };
 
-/* Returns wether c is a data movement character */
-static int is_movement(char c)
+static char get(struct Interpreter *i, int row, int col)
 {
-    switch(c)
+    assert(row >= 0 && row < i->fld_sz.height &&
+           col >= 0 && col < i->fld_sz.width);
+    return i->field[i->fld_cap.width*row + col];
+}
+
+static void set(struct Interpreter *i, int row, int col, char value)
+{
+    assert(row >= 0 && row < i->fld_sz.height &&
+           col >= 0 && col < i->fld_sz.width);
+    i->field[i->fld_cap.width*row + col] = value;
+}
+
+static void add(struct Interpreter *i, int row, int col, int value)
+{
+    assert(row >= 0 && row < i->fld_sz.height &&
+           col >= 0 && col < i->fld_sz.width);
+    i->field[i->fld_cap.width*row + col] += value;
+}
+
+static int cursor_needs_input(struct Interpreter *i, struct Cursor *c)
+{
+    if (c->dm != M_INPUT)
+        return 0;
+    switch (get(i, c->ir, c->ic))
     {
-    case '>':
     case '^':
+        return c->dr != 0;
+    case '>':
     case '<':
     case 'v':
     case 'X':
@@ -59,6 +82,217 @@ static void ensure(struct Interpreter *i, int height, int width)
         i->fld_sz.width = width;
     if (height > i->fld_sz.height)
         i->fld_sz.height = height;
+}
+
+static void set_effect(struct Interpreter *i, struct Cursor *c)
+{
+    switch(c->dm)
+    {
+    case M_NONE:
+        break;
+    case M_ADD:
+        c->effect = E_ADD | ((+get(i, c->dr, c->dc))&0xff);
+        break;
+    case M_SUBTRACT:
+        c->effect = E_ADD | ((-get(i, c->dr, c->dc))&0xff);
+        break;
+    case M_INPUT:
+        c->effect = E_INPUT;
+        break;
+    case M_OUTPUT:
+        {
+            int ch = get(i, c->dr, c->dc);
+            if (i->output == IO_NONE)
+                i->output = ch;
+            else
+            if (i->output != ch)
+                i->output = IO_BLOCK;
+            break;
+        }
+    case M_CLEAR:
+        c->effect = E_CLEAR;
+        break;
+    }
+}
+
+static void move_ip(struct Interpreter *i, struct Cursor *c)
+{
+    c->ir += DR[c->id];
+    c->ic = (c->ic + DC[c->id] + i->fld_sz.width)%i->fld_sz.width;
+}
+
+static struct Cursor **kill_cursor(struct Cursor **ptr)
+{
+    struct Cursor *c = *ptr;
+    *ptr = c->next;
+    free(c);
+    return ptr;
+}
+
+static struct Cursor **fork_cursor(struct Interpreter *i, struct Cursor **ptr)
+{
+    struct Cursor *b = malloc(sizeof(struct Cursor)), *c = *ptr;
+    assert(b);
+    *b = *c;
+    b->next = c;
+    *ptr = b;
+    b->id ^= 1;
+    c->id ^= 3;
+    move_ip(i, b);
+    return &b->next;
+}
+
+/* Executes a single instruction and returns a pointer to
+   a pointer to the next cursor. */
+static struct Cursor **run_cursor(struct Interpreter *i, struct Cursor **ptr)
+{
+    struct Cursor *c = *ptr;
+
+    /* Reset field effect */
+    c->effect = 0;
+
+    /* Evaluate instruction */
+    switch (get(i, c->ir, c->ic))
+    {
+        /* Move data pointer */
+    case '>':
+        set_effect(i, c);
+        if (++c->dc == i->fld_sz.width)
+            c->dc = 0;
+        break;
+    case 'v':
+        set_effect(i, c);
+        if (++c->dr == i->fld_sz.height)
+            ensure(i, c->dr + 1, 0);
+        break;
+    case '<':
+        set_effect(i, c);
+        if (c->dc-- == 0)
+            c->dc = i->fld_sz.width - 1;
+        break;
+    case '^':
+        if (c->dr == 0)
+            return kill_cursor(ptr);
+        set_effect(i, c);
+        --c->dr;
+        break;
+    case 'X':
+        set_effect(i, c);
+        break;
+
+        /* Change data mode */
+    case '~': c->dm = M_NONE; break;
+    case '+': c->dm = M_ADD; break;
+    case '-': c->dm = M_SUBTRACT; break;
+    case '?': c->dm = M_INPUT; break;
+    case '!': c->dm = M_OUTPUT; break;
+    case '*':
+        if (i->flags & F_CLEAR_MODE)
+            c->dm = M_CLEAR;
+        break;
+
+        /* Change instruction pointer direction */
+    case '\\': c->id ^= 1; break;
+    case '/' : c->id ^= 3; break;
+    case '|' : c->id ^= 2; break;
+
+        /* Jumps */
+    case '#':
+        move_ip(i, c);
+        break;
+    case '@':
+        if (get(i, c->dr, c->dc) == 0)
+        {
+            c->ir += DR[c->id];
+            c->ic += DC[c->id];
+        }
+        break;
+
+        /* Fork */
+    case 'Y':
+        ptr = fork_cursor(i, ptr);
+        break;
+    }
+
+    /* Move instruction pointer */
+    move_ip(i, c);
+
+    return &c->next;
+}
+
+int interpreter_step(struct Interpreter *i, int in, int *out)
+{
+    struct Cursor **ptr, *c;
+    int result;
+
+    if (!i->cursors)
+        return I_EXIT;
+
+    result = I_SUCCESS;
+    i->output = IO_NONE;
+
+    /* Run cursors */
+    ptr = &i->cursors;
+    while (ptr && *ptr)
+        ptr = run_cursor(i, ptr);
+
+    /* Apply input read */
+    if (in >= 0 && in < 256)
+        for (c = i->cursors; c; c = c->next)
+            if ((c->effect&E_MASK) == E_INPUT)
+                set(i, c->dr, c->dc, in);
+
+    /* Apply additions/subtractions */
+    for (c = i->cursors; c; c = c->next)
+        if ((c->effect&E_MASK) == E_ADD)
+            add(i, c->dr, c->dc, c->effect);
+
+    /* Clear cells */
+    if (i->flags & F_CLEAR_MODE)
+    {
+        for (c = i->cursors; c; c = c->next)
+            if ((c->effect&E_MASK) == E_CLEAR)
+                set(i, c->dr, c->dc, 0);
+    }
+
+    /* Remove cursors with invalid IP */
+    ptr = &i->cursors;
+    while (ptr && *ptr)
+    {
+        c = *ptr;
+        if (c->ir < 0 || c->ir >= i->fld_sz.height)
+        {
+            ptr = kill_cursor(ptr);
+            continue;
+        }
+        if (cursor_needs_input(i, c))
+            result |= I_INPUT;
+        ptr = &(*ptr)->next;
+    }
+
+    /* Write output */
+    if (i->output < 256)
+    {
+        *out = i->output;
+        result |= I_OUTPUT;
+    }
+
+    return result;
+}
+
+struct Size interpreter_size(struct Interpreter *i)
+{
+    return i->fld_sz;
+}
+
+int interpreter_needs_input(struct Interpreter *i)
+{
+    struct Cursor *c;
+
+    for  (c = i->cursors; c; c = c->next)
+        if (cursor_needs_input(i, c))
+            return 1;
+    return 0;
 }
 
 struct Interpreter *interpreter_create()
@@ -141,238 +375,6 @@ void interpreter_destroy(struct Interpreter *i)
     free(i);
 }
 
-char interpreter_get(struct Interpreter *i, int row, int col)
-{
-    assert(row >= 0 && row < i->fld_sz.height &&
-           col >= 0 && col < i->fld_sz.width);
-    return i->field[i->fld_cap.width*row + col];
-}
-
-void interpreter_set(struct Interpreter *i, int row, int col, char value)
-{
-    assert(row >= 0 && row < i->fld_sz.height &&
-           col >= 0 && col < i->fld_sz.width);
-    i->field[i->fld_cap.width*row + col] = value;
-}
-
-void interpreter_add(struct Interpreter *i, int row, int col, int value)
-{
-    assert(row >= 0 && row < i->fld_sz.height &&
-           col >= 0 && col < i->fld_sz.width);
-    i->field[i->fld_cap.width*row + col] += value;
-}
-
-static void set_effect(struct Interpreter *i, struct Cursor *c)
-{
-    switch(c->dm)
-    {
-    case M_NONE:
-        break;
-    case M_ADD:
-        c->effect = E_ADD | ((+interpreter_get(i, c->dr, c->dc))&0xff);
-        break;
-    case M_SUBTRACT:
-        c->effect = E_ADD | ((-interpreter_get(i, c->dr, c->dc))&0xff);
-        break;
-    case M_INPUT:
-        c->effect = E_INPUT;
-        break;
-    case M_OUTPUT:
-        {
-            int ch = interpreter_get(i, c->dr, c->dc);
-            if (i->output == IO_NONE)
-                i->output = ch;
-            else
-            if (i->output != ch)
-                i->output = IO_BLOCK;
-            break;
-        }
-    case M_CLEAR:
-        c->effect = E_CLEAR;
-        break;
-    }
-}
-
-static void move_ip(struct Interpreter *i, struct Cursor *c)
-{
-    c->ir += DR[c->id];
-    c->ic = (c->ic + DC[c->id] + i->fld_sz.width)%i->fld_sz.width;
-}
-
-static struct Cursor **kill_cursor(struct Cursor **ptr)
-{
-    struct Cursor *c = *ptr;
-    *ptr = c->next;
-    free(c);
-    return ptr;
-}
-
-static struct Cursor **fork_cursor(struct Interpreter *i, struct Cursor **ptr)
-{
-    struct Cursor *b = malloc(sizeof(struct Cursor)), *c = *ptr;
-    assert(b);
-    *b = *c;
-    b->next = c;
-    *ptr = b;
-    b->id ^= 1;
-    c->id ^= 3;
-    move_ip(i, b);
-    return &b->next;
-}
-
-/* Executes a single instruction and returns a pointer to
-   a pointer to the next cursor. */
-static struct Cursor **run_cursor(struct Interpreter *i, struct Cursor **ptr)
-{
-    struct Cursor *c = *ptr;
-
-    /* Reset field effect */
-    c->effect = 0;
-
-    /* Evaluate instruction */
-    switch (interpreter_get(i, c->ir, c->ic))
-    {
-        /* Move data pointer */
-    case '>':
-        set_effect(i, c);
-        if (++c->dc == i->fld_sz.width)
-            c->dc = 0;
-        break;
-    case 'v':
-        set_effect(i, c);
-        if (++c->dr == i->fld_sz.height)
-            ensure(i, c->dr + 1, 0);
-        break;
-    case '<':
-        set_effect(i, c);
-        if (c->dc-- == 0)
-            c->dc = i->fld_sz.width - 1;
-        break;
-    case '^':
-        if (c->dr == 0)
-            return kill_cursor(ptr);
-        set_effect(i, c);
-        --c->dr;
-        break;
-    case 'X':
-        set_effect(i, c);
-        break;
-
-        /* Change data mode */
-    case '~': c->dm = M_NONE; break;
-    case '+': c->dm = M_ADD; break;
-    case '-': c->dm = M_SUBTRACT; break;
-    case '?': c->dm = M_INPUT; break;
-    case '!': c->dm = M_OUTPUT; break;
-    case '*':
-        if (i->flags & F_CLEAR_MODE)
-            c->dm = M_CLEAR;
-        break;
-
-        /* Change instruction pointer direction */
-    case '\\': c->id ^= 1; break;
-    case '/' : c->id ^= 3; break;
-    case '|' : c->id ^= 2; break;
-
-        /* Jumps */
-    case '#':
-        move_ip(i, c);
-        break;
-    case '@':
-        if (interpreter_get(i, c->dr, c->dc) == 0)
-        {
-            c->ir += DR[c->id];
-            c->ic += DC[c->id];
-        }
-        break;
-
-        /* Fork */
-    case 'Y':
-        ptr = fork_cursor(i, ptr);
-        break;
-    }
-
-    /* Move instruction pointer */
-    move_ip(i, c);
-
-    return &c->next;
-}
-
-int interpreter_step(struct Interpreter *i, int in, int *out)
-{
-    struct Cursor **ptr, *c;
-    int result;
-
-    if (!i->cursors)
-        return I_EXIT;
-
-    result = I_SUCCESS;
-    i->output = IO_NONE;
-
-    /* Run cursors */
-    ptr = &i->cursors;
-    while (ptr && *ptr)
-        ptr = run_cursor(i, ptr);
-
-    /* Apply input read */
-    if (in >= 0 && in < 256)
-        for (c = i->cursors; c; c = c->next)
-            if ((c->effect&E_MASK) == E_INPUT)
-                interpreter_set(i, c->dr, c->dc, in);
-
-    /* Apply additions/subtractions */
-    for (c = i->cursors; c; c = c->next)
-        if ((c->effect&E_MASK) == E_ADD)
-            interpreter_add(i, c->dr, c->dc, c->effect);
-
-    /* Clear cells */
-    if (i->flags & F_CLEAR_MODE)
-    {
-        for (c = i->cursors; c; c = c->next)
-            if ((c->effect&E_MASK) == E_CLEAR)
-                interpreter_set(i, c->dr, c->dc, 0);
-    }
-
-    /* Remove cursors with invalid IP */
-    ptr = &i->cursors;
-    while (ptr && *ptr)
-    {
-        c = *ptr;
-        if (c->ir < 0 || c->ir >= i->fld_sz.height)
-        {
-            ptr = kill_cursor(ptr);
-            continue;
-        }
-        if (c->dm == M_INPUT && is_movement(interpreter_get(i, c->ir, c->ic)))
-            result |= I_INPUT;
-        ptr = &(*ptr)->next;
-    }
-
-    /* Write output */
-    if (i->output < 256)
-    {
-        *out = i->output;
-        result |= I_OUTPUT;
-    }
-
-    return result;
-}
-
-struct Size interpreter_size(struct Interpreter *i)
-{
-    return i->fld_sz;
-}
-
-int interpreter_needs_input(struct Interpreter *i)
-{
-    struct Cursor *c;
-
-    for  (c = i->cursors; c; c = c->next)
-        if (c->dm == M_INPUT && is_movement(interpreter_get(i, c->ir, c->ic)))
-            return 1;
-    return 0;
-}
-
 struct Interpreter *interpreter_from_source(const char *filepath, char nul)
 {
     struct Interpreter *i;
@@ -403,13 +405,29 @@ struct Interpreter *interpreter_from_source(const char *filepath, char nul)
                 continue;
             }
             ensure(i, row + 1, col + 1);
-            interpreter_set(i, row, col, buf[n] == nul ? 0 : buf[n]);
+            set(i, row, col, buf[n] == nul ? 0 : buf[n]);
             col += 1;
         }
     }
     fclose(fp);
     return i;
 }
+
+char interpreter_get(struct Interpreter *i, int row, int col)
+{
+    return get(i, row, col);
+}
+
+void interpreter_set(struct Interpreter *i, int row, int col, char value)
+{
+    return set(i, row, col, value);
+}
+
+void interpreter_add(struct Interpreter *i, int row, int col, int value)
+{
+    return add(i, row, col, value);
+}
+
 
 int interpreter_get_flags(struct Interpreter *i)
 {
